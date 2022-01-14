34ee6caa02b8aed8afb2bf61c6d2871707703c5a
Nick Otto <nick83487@gmail.com>
2022-01-14 12:59:34 -0600

    Updated Hangman to add lives

--- Small Python Projects/Hangman/Hangman Game.py
+++ Small Python Projects/Hangman/Hangman Game.py
@@ -16,14 +16,9 @@ def hangman():
     word_letters = set(word) #Letters in the target word
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #Letters that have been guessed
+    lives = 8 #Amount of incorrect guesses the user has left
 
-    while len(word_letters) > 0:
-        # #letters used
-        # print("You have used these letters:", ' '.join(used_letters))
-
-        # #what their guesses look like so far
-        # word_list = [letter if letter in used_letters else '-' for letter in word]
-        # print('Current word:', ' '.join(word_list))
+    while len(word_letters) > 0 and lives > 0:
 
         #getting user input
         user_letter = input("Guess a letter: ").upper()
@@ -33,13 +28,15 @@ def hangman():
             #if the guessed letter is in the word, remove the letter from the 'word_letters' set
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives -= 1 #Incorrect guess takes away a life.
         elif (user_letter in used_letters):
             print("You have already guessed that letter! Please try again.")
         else:
             print("Invalid character.")
         
-        #letters used
-        print("You have used these letters:", ' '.join(used_letters))
+        #letters used and lives left
+        print("You have", lives, "lives left and you have used these letters:", ' '.join(used_letters))
 
         #what their guesses look like so far
         word_list = [letter if letter in used_letters else '-' for letter in word]
