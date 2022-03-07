27bac1983b981efad0137f9aa59533457542512e
Nick Otto <nick83487@gmail.com>
2022-03-07 15:48:12 -0600

    Stores data from price increases and decreases separately and calculates the average increase and decrease.

--- Overnight Gappers/Overnight Gappers.py
+++ Overnight Gappers/Overnight Gappers.py
@@ -9,14 +9,20 @@ def calcGap(row1, row2):
     change = (open - close) / close * 100 #Calculates the percent change in price overnight
     return change
 
-df = yf.download('SPY', datetime(2008, 1, 1), datetime(2009, 1, 1))
+df = yf.download('SPY', datetime(2000, 1, 1), datetime(2021, 1, 1))
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 i = 0
 j = 1
 num = 0 #Variable to track the amount of times the price went down by >1% overnight in the time period.
+numIncrease = 0
+numDecrease = 0
 avg = 0 #The average change in the price of SPY the day after the price gapped down.
+avgIncrease = 0
+avgDecrease= 0
 result = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+increase = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+decrease = pd.DataFrame(columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
 
 while (i < len(df) - 1):
     row1 = df.iloc[[i]] #Gets the row at index i of DataFrame df
@@ -34,14 +40,37 @@ while (i < len(df) - 1):
         result = result.append(row1)
         result = result.append(row2)
         change = (close - open) / open * 100 #The change in price of SPY the day after the price gapped down in %.
+
+        if (change < 0):
+            decrease = result.append(row1)
+            decrease = result.append(row2)
+            avgDecrease +=change
+            numDecrease += 1
+        elif (change > 0):
+            increase = result.append(row1)
+            increase = result.append(row2)
+            avgIncrease += change
+            numIncrease += 1
+
         avg += change
-        print(f"{round(change, 2)}%")
+        #print(f"{round(change, 2)}%")
         num += 1
     i += 1
     j += 1
 
 if (avg == 0):
     raise SystemExit("Error: Price never gapped down by more than 1 percent in the selected data.")
+
 avg = round(avg / num, 2)
-print(result)
+avgDecrease = round(avgDecrease / numDecrease, 2)
+avgIncrease = round(avgIncrease / numIncrease, 2)
+
+print("Days the price decreased: ")
+print(decrease)
+
+print("Days the price increased: ")
+print(increase)
+
 print(f"Average change in price: {avg}%")
+print(f"Average decrease in price: {avgDecrease}%")
+print(f"Average increase in price: {avgIncrease}%")
